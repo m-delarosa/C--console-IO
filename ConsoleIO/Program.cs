aa33f317389e009f845bccdb5574b990f190b442
github username <56710845+m-delarosa@users.noreply.github.com>
2021-01-03 20:08:00 -0700

    Add intiial commit

---
+++ ConsoleIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, what's your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("How about your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Great, we'll record your name as {firstName} {lastName}.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+            //Pause, will not continue until you hit enter.
+            //Console.ReadLine();
+        }
+    }
+}
